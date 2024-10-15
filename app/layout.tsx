423b4d2dfa67a034f1020a1d8b3ff1c34552bbe3
addisale27 <163672380+addisale27@users.noreply.github.com>
2024-10-15 23:39:53 +0300

    flexiblity error fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
         />
         <div className="flex flex-col min-h-screen">
           <NavBar />
-          <main className="flex-grow w-full">{children}</main>
+          <main className="flex-grow w-full overflow-hidden">{children}</main>
           <Footer />
         </div>
       </body>
